da6bc896f3eaeadce3e10f78d4247a9a40ed1196
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-03-25 08:26:51 +0300

    Change App.main()

--- spring-intro/src/main/java/com/epam/tickets/App.java
+++ spring-intro/src/main/java/com/epam/tickets/App.java
@@ -2,20 +2,25 @@ package com.epam.tickets;
 
 import com.epam.tickets.facade.BookingFacadeImpl;
 import com.epam.tickets.model.dto.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+
+  private static final Logger logger = LogManager.getLogger(App.class);
 
   public static void main(String[] args) {
 
     ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
     BookingFacadeImpl facade = (BookingFacadeImpl) ctx.getBean("facade");
 
-    System.out.println("Ticket service.");
+    logger.warn("== Ticket service started ==");
 
     User user = new User("Alex", "al@gmail.com");
-    User user_ = facade.createUser(user);
-    System.out.println(user_.toString());
+    User createdUser = facade.createUser(user);
+    String userString = createdUser.toString();
+    logger.info(userString);
   }
 }
