d0fe70aeb99740cb6e983025429458a6d25bc924
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-05 08:41:11 +0300

    Update user controllers

--- spring-web/src/main/java/com/epam/tickets/controllers/UsersController.java
+++ spring-web/src/main/java/com/epam/tickets/controllers/UsersController.java
@@ -3,6 +3,7 @@ package com.epam.tickets.controllers;
 import com.epam.tickets.exceptions.InvalidUserException;
 import com.epam.tickets.facade.BookingFacade;
 import com.epam.tickets.model.dto.User;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -17,24 +18,64 @@ public class UsersController {
   @Autowired
   BookingFacade facade;
 
-  @GetMapping
-  public String index(ModelMap model, @RequestParam String id, @RequestParam String id2) {
-    model.addAttribute("message", id + ":" + id2);
-
-    return "user-template";
+  @GetMapping("/")
+  public String infoPage(ModelMap model) {
+    model.addAttribute("message", "User informatin page");
+    return "users-template";
   }
 
-  @GetMapping("/adduser")
-  public String addUser(ModelMap model, @RequestParam String name, @RequestParam String email) throws InvalidUserException {
+  @GetMapping("/add")
+  public String addUser(ModelMap model,
+      @RequestParam(required = false, defaultValue = "name") String name,
+      @RequestParam(required = false, defaultValue = "email") String email) throws InvalidUserException {
+
     User user = new User(name, email);
-    facade.createUser(user);
-    model.addAttribute("message", name + ":" + email);
-    return "user-template";
+    User createdUser = facade.createUser(user);
+    model.addAttribute("message", "User created: " + createdUser.toString());
+    return "users-create-template";
   }
 
-  @GetMapping("/all")
-  public String getAllUsers(ModelMap model) {
-    model.addAttribute("message", "Users-all");
-    return "user-template";
+  @GetMapping("/getById")
+  public String getUserById(ModelMap model, @RequestParam Long id) throws InvalidUserException {
+    User user = facade.getUserById(id);
+    model.addAttribute("message", "User found: " + user.toString());
+    return "users-template";
+  }
+
+  @GetMapping("/getByEmail")
+  public String getUserByEmail(ModelMap model, @RequestParam String email) throws InvalidUserException {
+    User user = facade.getUserByEmail(email);
+    model.addAttribute("message", "User found: " + user.toString());
+    return "users-template";
+  }
+
+  @GetMapping("/getByName")
+  public String getUsersByName(ModelMap model,
+      @RequestParam String name,
+      @RequestParam(required = false, defaultValue = "10") int pageSize,
+      @RequestParam(required = false, defaultValue = "0") int pageNum) {
+
+    List<User> usersByName = facade.getUsersByName(name, pageSize, pageNum);
+    String message = "Users found by name: " + usersByName;
+    model.addAttribute("message", message);
+    return "users-template";
+  }
+
+  @GetMapping("/update")
+  public String updaetUser(ModelMap model,
+      @RequestParam Long userId,
+      @RequestParam String userName,
+      @RequestParam String userEmail) throws InvalidUserException {
+    User user = new User(userId, userName, userEmail);
+    facade.updateUser(user);
+    model.addAttribute("message", "User is updated");
+    return "users-template";
+  }
+
+  @GetMapping("/delete")
+  public String deleteUser(ModelMap model, @RequestParam Long userId) throws InvalidUserException {
+    facade.deleteUserById(userId);
+    model.addAttribute("message", "User deleted");
+    return "users-template";
   }
 }
---
+++ spring-web/src/main/webapp/WEB-INF/views/booked-tickets-template.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Spring MVC XML Configuration Example</title>
+</head>
+<body>
+<h3>Get Booked Tickets</h3>
+
+${message}
+
+</body>
+</html>
