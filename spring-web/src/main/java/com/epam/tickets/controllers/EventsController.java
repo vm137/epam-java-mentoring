9396458d783ae48a831f9eed2f2724dae67d5727
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-06 22:36:09 +0300

    Update EventsController

--- spring-web/src/main/java/com/epam/tickets/controllers/EventsController.java
+++ spring-web/src/main/java/com/epam/tickets/controllers/EventsController.java
@@ -5,45 +5,73 @@ import com.epam.tickets.model.dto.Event;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/event")
+@RequestMapping("/events")
 public class EventsController {
 
   @Autowired
   BookingFacade facade;
 
+  private static final Logger logger = LogManager.getLogger(EventsController.class);
+
   @GetMapping("/")
-  public String infoPage(ModelMap model) {
-    model.addAttribute("message", "Event Information Page");
-    return "events/events-info";
+  public String getAllEvents(ModelMap model,
+      @RequestParam(required = false, defaultValue = "") String title,
+      @RequestParam(required = false, defaultValue = "") String date) {
+    List<Event> events;
+    if (StringUtils.isNotBlank(title)) {
+      events = facade.getEventsByTitle(title, 10, 0);
+      model.addAttribute("message", "Events by title: " + title);
+    } else if (StringUtils.isNotBlank(date)) {
+      DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+      LocalDateTime dateForSearch = LocalDateTime.parse("12/02/2020 12:00", formatter);
+      events = facade.getEventsForDay(dateForSearch, 10, 0);
+      model.addAttribute("message", "Events by date: " + date);
+    } else {
+      events= facade.getAllEvents();
+      model.addAttribute("message", "All Events");
+    }
+    model.addAttribute("events", events);
+    return "events/show-events";
   }
 
-  @GetMapping("/add")
-  public String createEvent(ModelMap model, @RequestParam String title, String date) {
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-    LocalDateTime dateParsed = LocalDateTime.parse(date, formatter);
-    Event event = new Event(title, dateParsed);
+  @GetMapping("/{id}")
+  public String infoPage(ModelMap model,
+      @PathVariable Long id) {
+    model.addAttribute("message", "Event Information");
+    Event event = facade.getEventById(id);
+    model.addAttribute("event", event);
+    return "events/show-event";
+  }
+
+  @PostMapping("/add")
+  public String createEvent(ModelMap model,
+      @RequestParam String title,
+      @RequestParam String date) {
+    Event event = new Event(title, parseDate(date));
     Event eventCreated = facade.createEvent(event);
+    String msg = String.format("Event with id:%d, title: '%s', date: %s is created.",
+        eventCreated.getId(), eventCreated.getTitle(), eventCreated.getDate().toString());
+    logger.info(msg);
     model.addAttribute("message", "Event created");
     model.addAttribute("event", eventCreated);
     return "events/show-event";
   }
 
-  @GetMapping("/getByTitle")
-  public String getEventByTitle(ModelMap model,
-      @RequestParam String title,
-      @RequestParam(required = false, defaultValue = "10") int pageSize,
-      @RequestParam(required = false, defaultValue = "1") int pageNum) {
-    List<Event> events = facade.getEventsByTitle(title, pageSize, pageNum);
-    model.addAttribute("message", "Event Information Page");
-    model.addAttribute("events", events);
-    return "events/show-events";
+  private LocalDateTime parseDate(String date) {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+    return LocalDateTime.parse(date, formatter);
   }
 }
