2e588e8aff2053557043ac9a1bc9c396af98e803
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-06 22:38:04 +0300

    Delete unused views

--- spring-web/src/main/java/com/epam/tickets/controllers/EventsController.java
+++ spring-web/src/main/java/com/epam/tickets/controllers/EventsController.java
@@ -1,5 +1,6 @@
 package com.epam.tickets.controllers;
 
+import com.epam.tickets.exceptions.InvalidEventException;
 import com.epam.tickets.facade.BookingFacade;
 import com.epam.tickets.model.dto.Event;
 import java.time.LocalDateTime;
@@ -11,7 +12,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +30,7 @@ public class EventsController {
   private static final Logger logger = LogManager.getLogger(EventsController.class);
 
   @GetMapping("/")
-  public String getAllEvents(ModelMap model,
+  public String getEvents(ModelMap model,
       @RequestParam(required = false, defaultValue = "") String title,
       @RequestParam(required = false, defaultValue = "") String date) {
     List<Event> events;
@@ -48,8 +51,7 @@ public class EventsController {
   }
 
   @GetMapping("/{id}")
-  public String infoPage(ModelMap model,
-      @PathVariable Long id) {
+  public String showEvent(ModelMap model, @PathVariable Long id) {
     model.addAttribute("message", "Event Information");
     Event event = facade.getEventById(id);
     model.addAttribute("event", event);
@@ -69,6 +71,28 @@ public class EventsController {
     model.addAttribute("event", eventCreated);
     return "events/show-event";
   }
+
+  @PatchMapping("/{id}")
+  public String updateEvent(ModelMap model,
+      @PathVariable Long id,
+      @RequestParam String title,
+      @RequestParam String date) throws InvalidEventException {
+    Event event = new Event(id, title, parseDate(date));
+    facade.updateEvent(event);
+    logger.info(String.format("Event with id:%d is updated.", id));
+    model.addAttribute("message", "Event updated");
+    model.addAttribute("event", event);
+    return "events/show-event";
+  }
+
+    @DeleteMapping("/{id}")
+    public String deleteEvent(ModelMap model,
+        @PathVariable Long id) {
+      facade.deleteEventById(id);
+      logger.info(String.format("Event with id:%d is deleted.", id));
+      model.addAttribute("message", "Event deleted");
+      return "info/show-info";
+    }
 
   private LocalDateTime parseDate(String date) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
