80dcbdd56f42cc53b56c457d2e84b00f5c6b45c4
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-06 20:22:10 +0300

    Update Web Configuration

---
+++ spring-web/src/main/java/com/epam/tickets/config/WebConfiguration.java
@@ -0,0 +1,51 @@
+package com.epam.tickets.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+class WebConfiguration {
+
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  @Bean
+  public SpringResourceTemplateResolver templateResolver() {
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(this.applicationContext);
+    templateResolver.setPrefix("/WEB-INF/templates/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCacheable(true);
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
+  }
+
+  @Bean
+  @Description("Thymeleaf View Resolver")
+  public ThymeleafViewResolver viewResolver() {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(templateEngine());
+    viewResolver.setOrder(1);
+    return viewResolver;
+  }
+}
+
+/*
+ * Source:
+ * https://github.com/eugenp/tutorials/tree/master/spring-web-modules/spring-thymeleaf/src/main/java/com/baeldung
+ */
