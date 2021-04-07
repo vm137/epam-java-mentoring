455bf7b3a4d37fbb51fe38390b23d0227fcfc9d6
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-07 18:47:32 +0300

    Update Tickets controller

--- spring-web/src/main/java/com/epam/tickets/controllers/TicketsController.java
+++ spring-web/src/main/java/com/epam/tickets/controllers/TicketsController.java
@@ -1,25 +1,59 @@
 package com.epam.tickets.controllers;
 
+import com.epam.tickets.exceptions.InvalidEventException;
 import com.epam.tickets.exceptions.InvalidTicketException;
+import com.epam.tickets.exceptions.InvalidUserException;
 import com.epam.tickets.facade.BookingFacade;
+import com.epam.tickets.model.dto.Event;
 import com.epam.tickets.model.dto.Ticket;
 import com.epam.tickets.model.dto.TicketI.Category;
+import com.epam.tickets.model.dto.User;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/ticket")
+@RequestMapping("/tickets")
 public class TicketsController {
 
   @Autowired
   BookingFacade facade;
 
-  @GetMapping("/add")
-  public String createTicket(ModelMap model,
+  private static final Logger logger = LogManager.getLogger(TicketsController.class);
+
+  @GetMapping("/")
+  public String getTickets(ModelMap model,
+      @RequestParam(required = false, defaultValue = "0") Long userId,
+      @RequestParam(required = false, defaultValue = "0") Long eventId)
+      throws InvalidUserException, InvalidEventException {
+    if (userId != 0) {
+      User user = facade.getUserById(userId);
+      List<Ticket> bookedTickets = facade.getBookedTickets(user, 10, 0);
+      model.addAttribute("message", "Requested tickets:");
+      model.addAttribute("tickets", bookedTickets);
+      return "tickets/show-tickets";
+    } else if (eventId != 0) {
+      Event event = facade.getEventById(eventId);
+      List<Ticket> bookedTickets = facade.getBookedTickets(event, 10, 0);
+      model.addAttribute("message", "Requested tickets:");
+      model.addAttribute("tickets", bookedTickets);
+      return "tickets/show-tickets";
+    } else {
+      model.addAttribute("message", "Tickets not found.");
+      return "info/show-info";
+    }
+  }
+
+  @GetMapping("/book")
+  public String bookTicket(ModelMap model,
       @RequestParam Long userId,
       @RequestParam Long eventId,
       @RequestParam int place,
@@ -30,10 +64,12 @@ public class TicketsController {
     return "/tickets/show-ticket";
   }
 
-  @GetMapping("/getById")
-  public String cancelTicket(ModelMap model, @RequestParam Long id) throws InvalidTicketException {
+  @DeleteMapping("/{id}")
+  public String deleteEvent(ModelMap model,
+      @PathVariable Long id) throws InvalidTicketException {
     facade.cancelTicket(id);
-    model.addAttribute("message", "Ticket with id: " + id + " canceled.");
-    return "/tickets/show-ticket";
+    logger.info(String.format("Ticket with id:%d is deleted.", id));
+    model.addAttribute("message", "Ticket deleted");
+    return "info/show-info";
   }
 }
