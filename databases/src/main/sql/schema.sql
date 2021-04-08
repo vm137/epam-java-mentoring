edc6480e7c165995eb899dc416203f20015ee9ca
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-08 10:44:04 +0300

    Update schema and test-data

--- databases/src/main/sql/schema.sql
+++ databases/src/main/sql/schema.sql
@@ -4,8 +4,8 @@ drop table if exists students;
 create table students
 (
     id       integer generated always as identity
-        constraint students_pkey
-            primary key,
+        constraint students_pkey primary key,
+    code integer unique,
     first       varchar not null,
     last        varchar not null,
     date_of_birth    date,
@@ -20,8 +20,8 @@ drop table if exists subjects;
 create table subjects
 (
     id integer generated always as identity
-        constraint subjects_pkey
-            primary key,
+        constraint subjects_pkey primary key,
+    code       integer unique,
     title      varchar,
     tutor      varchar,
     hours      integer
@@ -31,14 +31,11 @@ drop table if exists exam_results;
 create table exam_results
 (
     id    integer generated always as identity
-        constraint exam_results_pkey
-            primary key,
-    student_id integer not null
-        constraint fk_student
-            references students,
-    subject_id integer not null
-        constraint fk_subject
-            references subjects,
+        constraint exam_results_pkey primary key,
+    student_code integer not null
+        constraint fk_student references students(code),
+    subject_code integer not null constraint fk_subject
+            references subjects(code),
     mark       varchar not null
 );
 
@@ -46,7 +43,8 @@ create table exam_results
 create index name on students using hash (last);
 
 -- update timestamp function
-create or replace function update_modified_column() returns trigger
+create or replace function update_modified_column()
+    returns trigger
     language plpgsql as
 $$
 BEGIN
@@ -56,17 +54,16 @@ END;
 $$;
 
 create trigger update_customer_modtime
-    before update
-    on students
-    for each row
+    before update on students for each row
     execute procedure update_modified_column();
 
--- data
-insert into cdp.public.students (first, last, primary_skill)
-values ('John', 'Dow', 'programmer'),
-       ('Sofi', 'Laurent', 'singer'),
-       ('Arthur', 'Conan Doyle', 'writer');
-
-update students set first='Sofi-2', last = 'Loren-2' where id = 2;
-
 -- validation
+alter table students
+    add constraint ck_No_Special_Characters
+        check (students.first not similar to '%[@#$]%' and
+               students.last not similar to '%[@#$]%');
+
+-- ALTER TABLE students DROP CONSTRAINT ck_No_Special_Characters;
+
+-- function average_mark()
+create or replace function average_mark()
--- databases/src/main/sql/test-data.sql
+++ databases/src/main/sql/test-data.sql
@@ -1,4 +1,25 @@
-insert into cdp.public.students (first, last, primary_skill)
-values ('John', 'Dow', 'programmer'),
-       ('Sofi', 'Laurent', 'singer'),
-       ('Arthur', 'Conan Doyle', 'writer');
+insert into cdp.public.students (code, first, last, primary_skill) values
+(101, 'John', 'Dow', 'programmer'),
+(102, 'Sophia', 'Loren', 'singer'),
+(103, 'James', 'Smith', 'analyst'),
+(104, 'Maria', 'Rodriguez', 'housewife'),
+(105, 'Ralph', 'Lauren', 'designer'),
+(106, 'Arthur', 'Conan Doyle', 'writer');
+
+-- update students set first = 'Sofi-2', last = 'Loren-2' where code = 102;
+
+insert into cdp.public.subjects (code, title, tutor, hours) values
+(108, 'Mathematics', 'Rene Descartes', 100),
+(205, 'Software Engineering', 'Eric Gamma', 50),
+(107, 'Compilers', 'Martin Fowler', 75),
+(404, 'Type Theory', 'Alan Turing', 55),
+(132, 'Clean Code', 'Robert Martin', 20),
+(435, 'Algorithms', 'Robert Sedgewick', 60);
+
+insert into exam_results (student_code, subject_code, mark) values
+(101, 108, 2), (101, 205, 3), (101, 107, 1),
+(102, 404, 5), (102, 132, 2), (102, 435, 3),
+(103, 205, 3), (103, 108, 5), (103, 404, 2),
+(104, 107, 4), (104, 108, 4), (104, 132, 4),
+(105, 205, 1), (105, 108, 1), (105, 107, 3),
+(106, 404, 2), (106, 205, 2), (106, 435, 5);
