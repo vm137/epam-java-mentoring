8be406aba2cf788f5267830461fe62cb70806d05
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-09 13:49:32 +0300

    Update Application

--- databases/src/main/java/Application.java
+++ databases/src/main/java/Application.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
@@ -7,28 +8,58 @@ public class Application {
 
   public static void main(String[] args) throws SQLException {
     Connection conn = Configuration.connect();
+    Statement stmt = conn.createStatement();
+    String sql;
+    ResultSet rs;
 
 //    a. 100K of users
-    Statement stmt = conn.createStatement();
+    String first;
+    String last;
     for (int i = 0; i < 100_000; i++) {
-      String first = randomString();
-      String last = randomString();
-      String sql = "INSERT INTO students "
+      first = randomString();
+      last = randomString();
+      sql = "INSERT INTO students "
           + "(code, first, last) values "
           + "(" + (220 + i) + ", '" + first + "', '" + last + "');";
       stmt.executeUpdate(sql);
     }
-    stmt.close();
 
 //    b. 1K of subjects
-//    for (int i = 0; i < 1000; i++) {
-//
-//    }
+    String title;
+    for (int i = 0; i < 1_000; i++) {
+      title = randomString();
+      sql = "INSERT INTO subjects "
+          + "(code, title) values "
+          + "(" + (500 + i) + ", '" + title + "');";
+      stmt.executeUpdate(sql);
+    }
 
-//    c. 1 million of marks
-//    for (int i = 0; i < 1_000_000; i++) {
-//
-//    }
+//    c. 1 20K of marks
+    Random random = new Random();
+    int student_code;
+    int subject_code;
+    for (int i = 0; i < 20_000; i++) {
+      // student_code
+      sql = "SELECT code FROM students ORDER BY RANDOM() LIMIT 1";
+      rs = stmt.executeQuery(sql);
+      rs.next();
+      student_code = rs.getInt("code");
+
+      // subject_code
+      sql = "SELECT code FROM subjects ORDER BY RANDOM() LIMIT 1";
+      rs = stmt.executeQuery(sql);
+      rs.next();
+      subject_code = rs.getInt("code");
+
+      // mark
+      int mark = random.nextInt(5) + 1;
+      sql = "INSERT INTO exam_results "
+          + "(student_code, subject_code, mark) values "
+          + "(" + student_code + ", " + subject_code + ", " + mark + ");";
+      stmt.executeUpdate(sql);
+    }
+
+    stmt.close();
     conn.close();
   }
 
