c10c0ae929999e65c94f84df201ae38320463686
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-04-24 20:22:22 +0300

    Add UserAccount services

--- spring-data/src/main/java/com/epam/tickets/services/UserAccountService.java
+++ spring-data/src/main/java/com/epam/tickets/services/UserAccountService.java
@@ -2,5 +2,7 @@ package com.epam.tickets.services;
 
 public interface UserAccountService {
 
-  void refillUserAccount(Long userId, int amount);
+  void refill(Long userId, int amount);
+
+  void withdraw(Long userId, int amount);
 }
