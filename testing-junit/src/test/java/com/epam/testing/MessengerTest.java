d2cd4c553fd6a27c23efa80deee364e4b8207bf5
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-06-22 13:41:43 +0300

    MessengerTest

--- testing-junit/src/test/java/com/epam/testing/MessengerTest.java
+++ testing-junit/src/test/java/com/epam/testing/MessengerTest.java
@@ -1,5 +1,44 @@
 package com.epam.testing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.epam.testing.template.Template;
+import com.epam.testing.template.TemplateEngine;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class MessengerTest {
 
+  MailServer mailServer;
+  TemplateEngine templateEngine;
+  Messenger messenger;
+  Client client;
+
+  @BeforeEach
+  private void init() {
+    mailServer = new MailServer();
+    templateEngine = new TemplateEngine();
+    messenger = new Messenger(mailServer, templateEngine);
+    client = new Client();
+  }
+
+  @Test
+  public void sendMessageTest() {
+    client.setAddresses("address-1");
+    HashMap<String, String> variables = new HashMap<>();
+    variables.put("name", "Victor");
+    variables.put("event", "Fashion Show");
+    client.setVariables(variables);
+
+    Template template = new Template();
+    template.setTemplate("Dear #{name}, we'd like to invite you to #{event}.");
+
+    messenger.sendMessage(client, template);
+    String messageSent = mailServer.getMessageSent();
+
+    String expected = "Dear Victor, we'd like to invite you to Fashion Show.";
+
+    assertEquals(messageSent, expected);
+  }
 }
