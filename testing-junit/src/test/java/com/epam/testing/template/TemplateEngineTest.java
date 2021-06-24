9ab122b4a78aba0b3374fcb5a1c8f3e0f9954ee3
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-06-24 18:37:49 +0300

    Add dynamic test

--- testing-junit/src/test/java/com/epam/testing/template/TemplateEngineTest.java
+++ testing-junit/src/test/java/com/epam/testing/template/TemplateEngineTest.java
@@ -1,5 +1,41 @@
 package com.epam.testing.template;
 
-class TemplateEngineTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import com.epam.testing.Client;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+class TemplateEngineTest {
+  Client client;
+  Template template;
+  HashMap<String, String> variables;
+
+  @BeforeEach
+  public void init() {
+    client = new Client();
+    template = new Template();
+    variables = new HashMap<>();
+  }
+
+  @Test
+  void generateSimpleMessageTest() {
+    TemplateEngine engine = new TemplateEngine();
+    variables.put("one", "fox");
+    variables.put("two", "dog");
+    client.setVariables(variables);
+    template.setTemplate("one: #{one}, two: #{two}");
+    String expected = "one: fox, two: dog";
+
+    String generated = engine.generateMessage(client, template);
+    assertEquals(expected, generated);
+  }
+
+
+  @TestFactory
+  void generateMessageTest() {
+
+  }
 }
