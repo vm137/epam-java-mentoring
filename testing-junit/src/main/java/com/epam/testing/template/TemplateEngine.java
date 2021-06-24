24e7075eed964829456da8c3b31c4cb3774d2241
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-06-24 20:24:40 +0300

    Add TemplateEngineException

--- testing-junit/src/main/java/com/epam/testing/template/TemplateEngine.java
+++ testing-junit/src/main/java/com/epam/testing/template/TemplateEngine.java
@@ -1,9 +1,12 @@
 package com.epam.testing.template;
 
 import com.epam.testing.Client;
+import exception.TemplateEngineException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * The type Template engine.
@@ -21,11 +24,27 @@ public class TemplateEngine {
         HashMap<String, String> variables = client.getVariables();
         Set<Entry<String, String>> entrySet = variables.entrySet();
 
+        if (!arePlaceholdersSupplied(template, variables)) {
+            throw new TemplateEngineException("Some placeholders don't have provided values.");
+        }
+
         for (Entry<String, String> entry : entrySet) {
             String pattern = "#\\{" + entry.getKey() + "}";
             message = message.replaceAll(pattern, entry.getValue());
         }
 
         return message;
+    }
+
+    private boolean arePlaceholdersSupplied(Template template, HashMap<String, String> variables) {
+        Pattern pattern = Pattern.compile("#\\{(.[^}]*)}");
+        Matcher matcher = pattern.matcher(template.getTemplate());
+        while(matcher.find()) {
+            String plh = matcher.group(1);
+            if (!variables.containsKey(plh)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
