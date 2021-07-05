23adaf7b630c4523eb04207ae07b1acfc17d0e46
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-07-05 21:00:46 +0300

    Add Task 4

--- concurrency/src/main/java/com/epam/concurrency/task4/BlockingObjectPool.java
+++ concurrency/src/main/java/com/epam/concurrency/task4/BlockingObjectPool.java
@@ -1,13 +1,22 @@
 package com.epam.concurrency.task4;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class BlockingObjectPool {
+
+  private BlockingQueue<Object> pool;
+  private final int max;
+  private final Object notEmpty = new Object();
+  private final Object notFull = new Object();
 
   /**
    * Creates filled pool of passed size
    * @param size of pool
    */
   public BlockingObjectPool(int size) {
-
+    this.max = size;
+    pool = new ArrayBlockingQueue<>(max);
   }
 
   /**
@@ -15,15 +24,31 @@ public class BlockingObjectPool {
    * @return object from pool
    */
   public Object get() {
-
-    return null;
+    if (pool.size() == 0) {
+      try {
+        notEmpty.wait();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    Object object = pool.remove();
+    notFull.notifyAll();
+    return object;
   }
 
   /**
    * Puts object to pool or blocks if pool is full
    * @param object to be taken back to pool
    */
-  public void take(Object object) {
-
+  public synchronized void put(Object object) {
+    while (pool.size() == max) {
+      try {
+        notFull.wait();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    pool.add(object);
+    notEmpty.notifyAll();
   }
 }
