8f7842d9fe642527afec0a43efac123f28d14617
Victor Mikhaylov <victor_mikhaylov@epam.com>
2021-07-06 17:00:23 +0300

    Updates after code review

--- java8/src/main/java/com/epam/java8/aggregator/Java8Aggregator.java
+++ java8/src/main/java/com/epam/java8/aggregator/Java8Aggregator.java
@@ -3,8 +3,6 @@ package com.epam.java8.aggregator;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javafx.util.Pair;
 
@@ -19,12 +17,10 @@ public class Java8Aggregator implements Aggregator {
     @Override
     public List<Pair<String, Long>> getMostFrequentWords(List<String> words, long limit) {
 
-        Set<Entry<String, Integer>> entries = words.stream()
+        return words.stream()
             .map(String::toLowerCase)
             .collect(Collectors.groupingBy(w -> w, Collectors.summingInt(w -> 1)))
-            .entrySet();
-
-        return entries
+            .entrySet()
             .stream()
             .map(entry -> new Pair<>(entry.getKey(), Long.valueOf(entry.getValue())))
             .collect(Collectors.toList());
